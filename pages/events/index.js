d89383c7af97051209ab985af03756c587613670
Klaus Dieter Born <95546468+Kdborn@users.noreply.github.com>
2023-05-21 18:05:31 +0200

    event search comp

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,27 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/event-search";
+import { getAllEvents } from "../../dummy-data";
+
 function AllEventsPage() {
+
+    const events = getAllEvents();
+    const router = useRouter();
+
+    function searchHandler(year,month) {
+
+        const fullpath = `/events/${year}/${month}`;
+        router.push(fullpath);
+
+    }
+
     return (
-        <div>
-            <h1>All Events</h1>
-        </div>
-    )
+        <Fragment>
+            <EventSearch onSearch={searchHandler}/>
+             <EventList items={events}/>
+        </Fragment>
+    );
 }
 
 export default AllEventsPage;
